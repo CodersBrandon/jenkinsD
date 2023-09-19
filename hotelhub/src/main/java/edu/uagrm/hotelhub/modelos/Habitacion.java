e7af14981740cba47974aefe4a54bd8389ab1a95
Renato Apaza <renato.apaza@outlook.com>
2023-09-19 10:25:42 -0400

    add modelos entidades

---
+++ hotelhub/src/main/java/edu/uagrm/hotelhub/modelos/Habitacion.java
@@ -0,0 +1,26 @@
+package edu.uagrm.hotelhub.modelos;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Data
+@Entity
+public class Habitacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipo; // Ejemplo: Simple, Doble, Suite
+    private double precioPorNoche;
+    private boolean disponible;
+
+    @ManyToOne
+    private Hotel hotel;
+
+    // Getters, Setters y otros métodos...
+}
